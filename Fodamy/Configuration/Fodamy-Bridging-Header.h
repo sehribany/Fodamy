f961c929708c20d01f2d0c15d6736c3e1af603be
Şehriban Yıldırım <sehribanyildirim051@gmail.com>
2023-12-06 17:08:50 +0300

    Create Fodamy-Bridging-Header

---
+++ Fodamy/Configuration/Fodamy-Bridging-Header.h
@@ -0,0 +1,10 @@
+//
+//  Fodamy-Bridging-Header.h
+//  Fodamy
+//
+//  Created by Şehriban Yıldırım on 30.11.2023.
+//
+
+#import <Utilities/Utilities.h>
+#import <UIComponents/UIComponents.h>
+#import <DataProvider/DataProvider.h>
