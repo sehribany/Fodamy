ace0aaf7077473ba0bda5a300bb3b630a8dc6646
Şehriban Yıldırım <sehribanyildirim051@gmail.com>
2023-12-06 17:15:23 +0300

    Create Pods

---
+++ Pods/Target Support Files/Pods-Fodamy-FodamyUITests/Pods-Fodamy-FodamyUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Fodamy_FodamyUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Fodamy_FodamyUITests
+@end
---
+++ Pods/Target Support Files/Pods-FodamyTests/Pods-FodamyTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FodamyTests : NSObject
+@end
+@implementation PodsDummy_Pods_FodamyTests
+@end
