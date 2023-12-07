480405aa50ae4557880a33ba5a0b0c88677f7be1
Şehriban Yıldırım <sehribanyildirim051@gmail.com>
2023-12-07 13:36:40 +0300

    Added DefaultsKey+Extensions

---
+++ Utilities/Utilities/Constants/DefaultsKey+Extensions.swift
@@ -0,0 +1,13 @@
+//
+//  DefaultsKey+Extensions.swift
+//  Utilities
+//
+//  Created by Şehriban Yıldırım on 1.12.2023.
+//
+
+import MobilliumUserDefaults
+
+public extension DefaultsKey{
+    static let isIntroCompleted = Key<Bool>(key: "isIntroCompleted")
+    static let userId           = Key<Int>(key: "userId")
+}
