2527973d4d92a5f12eaf06d357007393f47f0866
Şehriban Yıldırım <sehribanyildirim051@gmail.com>
2023-12-07 18:37:39 +0300

    Added APIRequestInterceptor

--- Fodamy/DataProvider/APIRequestInterceptor.swift
+++ Fodamy/DataProvider/APIRequestInterceptor.swift
@@ -13,13 +13,13 @@ public class APIRequestInterceptor: RequestInterceptor {
     public static let shared = APIRequestInterceptor()
     
     public func adapt(_ urlRequest: URLRequest, for session: Session, completion: @escaping (Result<URLRequest, Error>) -> Void) {
-        var urlRequest = urlRequest
+        var urlRequest  = urlRequest
         let accessToken = KeychainSwift().get(Keychain.token)
         
         if let accessToken = accessToken {
             urlRequest.headers.add(name: "X-Fodamy-Token", value: accessToken)
         }
-
+        
         completion(.success(urlRequest))
     }
 }
