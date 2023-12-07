44337ce91057f6a7cd90a50e2ae33a37f7147e46
Şehriban Yıldırım <sehribanyildirim051@gmail.com>
2023-12-07 13:36:58 +0300

    Added Keychain+Constants

---
+++ Utilities/Utilities/Constants/Keychain+Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Keychain+Constants.swift
+//  Utilities
+//
+//  Created by Şehriban Yıldırım on 1.12.2023.
+//
+
+import Foundation
+
+public struct Keychain {
+    public static let token = "token"
+}
