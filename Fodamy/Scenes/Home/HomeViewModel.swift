f87397c592826f634b57a5e5be3ea1735d6fb166
Şehriban Yıldırım <sehribanyildirim051@gmail.com>
2023-12-08 17:18:20 +0300

    Added HomeViewModel

--- Fodamy/Scenes/Home/HomeViewModel.swift
+++ Fodamy/Scenes/Home/HomeViewModel.swift
@@ -7,12 +7,19 @@
 
 import Foundation
 
-protocol HomeViewDataSource {}
+protocol HomeViewDataSource {
+    var segmentedControlTitles: [String] { get }
+    var selectedSegmentIndex  : Int { get }
+}
 
 protocol HomeViewEventSource {}
 
 protocol HomeViewProtocol: HomeViewDataSource, HomeViewEventSource {}
 
 final class HomeViewModel: BaseViewModel<HomeRouter>, HomeViewProtocol {
-    
+
+    var selectedSegmentIndex  : Int = 0
+    var segmentedControlTitles: [String] = [L10n.Modules.Home.editorChoiceRecipes,
+                                            L10n.Modules.Home.lastAddedRecipes]
 }
+
