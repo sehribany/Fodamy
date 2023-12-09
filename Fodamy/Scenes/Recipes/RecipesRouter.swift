ce36497152a46a2f65a3839fede8cb2a9a4d1e3c
Şehriban Yıldırım <sehribanyildirim051@gmail.com>
2023-12-09 12:51:26 +0300

    Added RecipesRouter

---
+++ Fodamy/Scenes/Recipes/RecipesRouter.swift
@@ -0,0 +1,10 @@
+//
+//  RecipesRouter.swift
+//  Fodamy
+//
+//  Created by Şehriban Yıldırım on 7.12.2023.
+//
+
+final class RecipesRouter: Router, RecipesRouter.Routes {
+    typealias Routes = RecipeDetailRoute
+}
