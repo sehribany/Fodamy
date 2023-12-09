b7aa785a3fb819b382f3bf01da0d49a7a90c11bc
Şehriban Yıldırım <sehribanyildirim051@gmail.com>
2023-12-09 12:46:40 +0300

    update HomeViewModel

--- Fodamy/Scenes/Home/HomeViewModel.swift
+++ Fodamy/Scenes/Home/HomeViewModel.swift
@@ -14,12 +14,22 @@ protocol HomeViewDataSource {
 
 protocol HomeViewEventSource {}
 
-protocol HomeViewProtocol: HomeViewDataSource, HomeViewEventSource {}
+protocol HomeViewProtocol: HomeViewDataSource, HomeViewEventSource {
+    func pushDetailViewController(recipe: Recipe)
+}
 
 final class HomeViewModel: BaseViewModel<HomeRouter>, HomeViewProtocol {
 
     var selectedSegmentIndex  : Int = 0
     var segmentedControlTitles: [String] = [L10n.Modules.Home.editorChoiceRecipes,
                                             L10n.Modules.Home.lastAddedRecipes]
+}
+
+// MARK: - Actions
+extension HomeViewModel {
+    
+    func pushDetailViewController(recipe: Recipe) {
+        router.pushRecipeDetail(recipe: recipe)
+    }
 }
 
