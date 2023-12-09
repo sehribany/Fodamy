ff1f30e2782f45bdf853db3a35cb3d1e6030305e
Şehriban Yıldırım <sehribanyildirim051@gmail.com>
2023-12-09 12:51:41 +0300

    Added RecipesViewController

---
+++ Fodamy/Scenes/Recipes/RecipesViewController.swift
@@ -0,0 +1,92 @@
+//
+//  RecipesViewController.swift
+//  Fodamy
+//
+//  Created by Şehriban Yıldırım on 7.12.2023.
+//
+
+import UIKit
+import UIComponents
+
+final class RecipesViewController: BaseViewController<RecipesViewModel> {
+    
+    private let collectionView: UICollectionView = {
+        let layout         = UICollectionViewFlowLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(RecipeCell.self, forCellWithReuseIdentifier: RecipeCell.identifier)
+        return collectionView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addSubViews()
+        configureContents()
+        viewModel.fetchRecipesListingType()
+        subscribeViewModelEvents()
+    }
+    
+    private func subscribeViewModelEvents() {
+        viewModel.didSuccessFetchRecipes = { [weak self] in
+            guard let self = self else { return }
+            self.collectionView.reloadData()
+        }
+    }
+    
+    private func addSubViews() {
+        addCollectionView()
+    }
+    
+    private func addCollectionView() {
+        view.addSubview(collectionView)
+        collectionView.edgesToSuperview()
+    }
+    
+    private func configureContents() {
+        collectionView.backgroundColor = Asset.Colors.appSecondaryBackground.color
+        collectionView.delegate        = self
+        collectionView.dataSource      = self
+    }
+    
+}
+
+extension RecipesViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        viewModel.showRecipeDetailScreen(at: indexPath)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return  viewModel.numberOfItemsAt(section: section)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell: RecipeCell = collectionView.dequeueReusableCell(withReuseIdentifier: RecipeCell.identifier, for: indexPath) as! RecipeCell
+        let cellItem = viewModel.cellItemAt(indexPath: indexPath)
+        cell.set(viewModel: cellItem)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 15
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return .zero
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = UIScreen.main.bounds.width
+        return CGSize(width: width, height: width + 145 )
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let position = scrollView.contentOffset.y
+        if position > collectionView.contentSize.height - 100 - scrollView.frame.size.height && viewModel.isPagingEnabled && viewModel.isRequestEnabled {
+            viewModel.fetchMorePages()
+        }
+    }
+}
