7807b9c5a92d3732b1b9cd4cb0ce07c4009ea43e
Şehriban Yıldırım <sehribanyildirim051@gmail.com>
2023-12-09 12:44:31 +0300

    Added SuccessResponse Model

---
+++ DataProvider/DataProvider/Model/SuccessResponse.swift
@@ -0,0 +1,10 @@
+//
+//  SuccessResponse.swift
+//  DataProvider
+//
+//  Created by Şehriban Yıldırım on 8.12.2023.
+//
+
+public struct SuccessResponse: Decodable {
+    public let message: String?
+}
