9aecd6deeeb5a080ba246d6a936eb146340b8875
Şehriban Yıldırım <sehribanyildirim051@gmail.com>
2023-12-10 13:28:09 +0300

    Added Closures

--- Utilities/Utilities/Closures/Closures.swift
+++ Utilities/Utilities/Closures/Closures.swift
@@ -5,10 +5,11 @@
 //  Created by Şehriban Yıldırım on 1.12.2023.
 //
 
-import Foundation
+import DataProvider
 
 public typealias VoidClosure      = (() -> Void)
 public typealias IntClosure       = ((Int) -> Void)
 public typealias BoolClosure      = ((Bool) -> Void)
 public typealias StringClosure    = ((String) -> Void)
 public typealias IndexPathClosure = ((IndexPath) -> Void)
+public typealias RecipeClosure    = ((Recipe) -> Void)
