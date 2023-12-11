77307cfb92884a917c9e4f243fdb582fb1ff4a3f
Şehriban Yıldırım <sehribanyildirim051@gmail.com>
2023-12-11 18:41:08 +0300

    Added NotificationNames

---
+++ Fodamy/Constants/NotificationNames.swift
@@ -0,0 +1,10 @@
+//
+//  NotificationNames.swift
+//  Fodamy
+//
+//  Created by Şehriban Yıldırım on 9.12.2023.
+//
+
+public extension Notification.Name {
+    static let reloadDetailView = Notification.Name("reloadDetailView")
+}
